a662d1eccc1c1d1214f434db6f6590fba18b11ef
ThomasStarphyre <tommspielermail@gmail.com>
2025-11-13 00:58:39 -0800

    Login Redirect for Demo

--- packages/react-frontend/src/Pages/Login.jsx
+++ packages/react-frontend/src/Pages/Login.jsx
@@ -1,33 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
-import axios from 'axios'; // fetches data from an API 
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from 'react-router-dom';
+import axios from 'axios'; // fetches data from an API
 import './Login.css';
 
 function Login(props) {
-  const [person, setPerson] = useState({name: ""});
+  const [person, setPerson] = useState({ name: '' });
 
   // checks when you input text into text boxes
   // then setPerson updates person
-  function handleChange(event){
+  function handleChange(event) {
     const { name, value } = event.target;
-    if (name == "job")
-        setPerson({ name: person["name"], job: value});
-    else setPerson({ name: value, job: person["job"]});
+    if (name == 'job') setPerson({ name: person['name'], job: value });
+    else setPerson({ name: value, job: person['job'] });
   }
 
-  function submitForm(){
+  function submitForm() {
     props.handleSubmit(person); // gives person to updateList
-    setPerson({ name: "", job: ""}); // reset after submission
-    }
+    setPerson({ name: '', job: '' }); // reset after submission
+  }
 
-    return (
+  return (
     <div className="login">
-      <div className="login-card">   {/* ← the rectangle */}
+      <div className="login-card">
+        {' '}
+        {/* ← the rectangle */}
         <h1>Login</h1>
         <form className="login-form">
           <input placeholder="Username" />
           <input type="password" placeholder="Password" />
-          <button>Sign in</button>
+          <button
+            type="button"
+            onClick={() => {
+              window.location.assign('/profile/690051534be4054686ca6757');
+            }}
+          >
+            Sign in
+          </button>
         </form>
         <p>
           Don't have an account? <Link to="/signup">Sign up here</Link>
