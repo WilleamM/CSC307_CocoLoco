f87ea8a43a0d5cf9d3e4df7d5ce2084d4d2ac46d
andresMen1274 <andresmendez1274@gmail.com>
2025-11-30 20:05:11 -0800

    Implement frontend Protected route

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import ProfilePage from './Pages/Profile_Page.jsx';
 import Login from './Pages/Login.jsx';
 import SignUp from './Pages/SignUp.jsx';
+const INVALID_TOKEN = 'INVALID_TOKEN';
 
 //This is needed for the fetch to work correctly it connects to the DB
 const API_PREFIX =
@@ -20,9 +21,18 @@ function Home() {
   );
 }
 
+//Used to login the user into their profile page
+function ProtectedRoute({ token, children }) {
+  if(token === 'INVALID_TOKEN'){
+    return <Navigate to="/login" replace/>;
+  }
+
+  return children;
+
+}
+
 // <Table characterData={characters}/> where characters is being passed to table as a prop
 function MyApp() {
-  const INVALID_TOKEN = 'INVALID_TOKEN';
   const [token, setToken] = useState(INVALID_TOKEN);
   const [message, setMessage] = useState('');
   const [characters, setCharacters] = useState(null);
@@ -139,31 +149,16 @@ function MyApp() {
           path="/signup"
           element={<SignUp handleSubmit={signupUser} buttonLabel="Sign Up" />}
         />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/profile/:userId" element={
+          <ProtectedRoute token={token}>
+             <ProfilePage />
+          </ProtectedRoute>
+            } 
+        />
         <Route path="*" element={<div>Not found</div>} />
       </Routes>
     </Router>
   );
 }
 
-//              AUTHENTICATION
-
-// Instructions say to make the below modification to useEffect() in MyApp(), but the original useEffect() was removed and MyApp()'s structure was changed.
-// However, it's unclear whether the below is necessary for authentication to function, so we may be able to get by without it.
-// If, in a future commit, authetication is working, these comments can be deleted.
-
-// Last existing useEffect() below
-// Deleted on commit 2089d5a
-/*
-useEffect(() => {
-  fetchUsers()
-    .then((res) => res.json())
-
-    .then((json) => setCharacters(json['users_list']))
-
-    .catch((error) => {
-      console.log(error);
-    });
-}, []);
-*/
 export default MyApp;
