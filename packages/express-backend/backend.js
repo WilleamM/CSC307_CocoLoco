348158d5a1332d8cc1a5f7673b3ac046023291b1
andresMen1274 <andresmendez1274@gmail.com>
2025-12-03 22:52:36 -0800

    Finished the friends page

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -176,17 +176,20 @@ app.post('/signup', registerUser);
 
 app.get('/feed', authenticateUser, async (req, res) => {
   try{
-      const userId = req.query.userId;
+      const userName = req.user.userName;
+      if(!userName){
+        return res.status(401).send('No user in the token');
+      }
 
-      const user = await userServices.findUserById(userId);
+      const user = await userServices.findUserByUserName(userName);
       if(!user){
         return res.status(404).send('User not found');
       }
 
+      const userId = user._id;
       const friendIds = user.friendIds || [];
 
       const authorsToShow = [userId, ...friendIds];
-
       const posts = await postServices.getPostByFriendIds(authorsToShow);
 
       res.send({posts_list: posts});
